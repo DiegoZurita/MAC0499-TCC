423f595ca35e96f95aa66324477bb7fe61880078
Diego Zurita <diego.zurita@usp.br>
2021-09-03 01:45:03 -0300

    Created log classifier

--- README.md
+++ README.md
@@ -33,3 +33,8 @@ Uma análise breve dos logs que estão em v2 para ver se foram gerados de acordo
 https://colab.research.google.com/drive/1LRwe-BydM45DsMw29IZBXmVbAboCRal6?usp=sharing
 
 O primeiro modelo foi criado. É uma ávore de decisão que, apesar de simples, consegue classificar todas as requisições ao DVWA como maliciosas e não maliciosas corretamente.
+
+### - Rodando o log classifier com o arquivo default
+```
+make logFile=/home/diego/Desktop/repos/tcc/projeto/dados/raw/jaron-honeynet-scan34/logs/access_log destFile=/home/diego/Desktop/repos/tcc/projeto/dados/processado/v4/access_log_classified.csv
+```
---
+++ projeto/scripts/log_classifier.py
@@ -0,0 +1,37 @@
+import os
+import linecache
+import sys
+
+
+def main():
+    print(sys.argv)
+    logFilePath = sys.argv[1]
+    destPathFile = sys.argv[2]
+    totalLines = int(sys.argv[3])
+    startFromLine = int(sys.argv[4])
+
+    if not os.path.isfile(destPathFile):
+        print("file not exists, creating..")
+        f = open(destPathFile, "w")
+        f.close()
+        
+
+    destfile = open(destPathFile, "r+")
+
+    print("Classifique as linhas abaixo (m = maliciosa, n = não maliciosa), q para sair")
+
+    for i in range(startFromLine, totalLines):
+        logLine = linecache.getline(logFilePath, i)
+        
+        classe = input("%s" % (logLine))
+
+        if classe == "q":
+            break
+
+        destfile.write("%d, %s, %s\n" % (i, logLine.replace("\n", ""), classe) )
+
+    destfile.close()
+    
+
+if __name__ == "__main__":
+    main()
